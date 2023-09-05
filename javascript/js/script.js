13e9bebc75d0db73f2e7bba02fc75518b939e135
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-09-05 20:34:06 -0300

    tipos de saida javascript

---
+++ javascript/js/script.js
@@ -0,0 +1,13 @@
+// SAÍDAS DO JAVASCRIPT: innerHTML, alert, console.log, document.write
+
+
+document.getElementById("texto").innerHTML="Olá, meu primeiro texto em <strong>Javascript</strong>. Mas para quebrar a maldição, vamos de <strong>HELLO, WORLD!</strong>";
+
+document.getElementById("date-now").innerHTML=Date()
+
+// alert("Oi, isso é um alerta! Meu primeiro 'alert' em Javascrip, então, HELLO, WORLD!!!")
+
+console.log("Oi, isso é um alerta! Meu primeiro 'console.log' em Javascrip, então, HELLO, WORLD!!!")
+
+// o innerHTML é uma das formas de "imprimir o conteúdo na tela"
+
